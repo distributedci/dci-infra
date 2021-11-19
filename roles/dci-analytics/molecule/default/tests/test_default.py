ea141eccc2579f9ceffb8952a01c01a6bebe6a80
ylamgarchal <yassine.lamgarchal@redhat.com>
2021-11-19 18:00:10 +0100

    dci-api: add elasticsearch url
    also add:
        - dci-analytics template settings
        - dci-analytcs molecule tests
    
    Change-Id: I7a35a762db1c68aa5b99d21ade2e47d8fa686aab

---
+++ roles/dci-analytics/molecule/default/tests/test_default.py
@@ -0,0 +1,18 @@
+"""Role testing files using testinfra."""
+
+
+def test_packages_installed(host):
+    assert host.package("dci-analytics").is_installed
+
+
+def test_services_is_running(host):
+    assert host.service("dci-analytics").is_running
+    assert host.service("dci-analytics-sync").is_enabled
+
+def test_api(host):
+    assert (
+        host.command(
+            "curl http://127.0.0.1:2345"
+        ).stdout
+        == '{"_status": "OK", "message": "Distributed CI Analytics"}'
+    )
